3fc586880375f9a483180ed3014b7e1ccbdb5712
Aleksandr Sadykov <sumerokr@gmail.com>
2020-11-13 10:55:42 +0100

    Tree shaking attempt 1

--- src/main.js
+++ src/main.js
@@ -1,17 +1,25 @@
-import * as components from "./components";
+// import * as components from "./components";
+export {
+  K24Button,
+  K24Button2,
+  K24Button3,
+  K24Button4,
+  K24Button5,
+  K24Button6,
+} from "./components";
 
-const ComponentLibrary = {
-  install(Vue) {
-    for (const componentName in components) {
-      const component = components[componentName];
+// const ComponentLibrary = {
+//   install(Vue) {
+//     for (const componentName in components) {
+//       const component = components[componentName];
 
-      Vue.component(component.name, component);
-    }
-  },
-};
+//       Vue.component(component.name, component);
+//     }
+//   },
+// };
 
-export default ComponentLibrary;
+// export default ComponentLibrary;
 
-if (typeof window !== "undefined" && window.Vue) {
-  window.Vue.use(ComponentLibrary);
-}
+// if (typeof window !== "undefined" && window.Vue) {
+//   window.Vue.use(ComponentLibrary);
+// }
